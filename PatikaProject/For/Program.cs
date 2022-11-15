9c9b26ca20796a02a4388de7c3369175cd5d92ba
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-15 23:47:00 +0300

    Add files via upload

---
+++ PatikaProject/For/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr= new int[10];
+
+        for (int i =0;i< arr.Length; i++)
+        {
+            if (i == 2)
+                arr[i]=i*24;
+        }
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
---
+++ PatikaProject/while-for each/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int timer = 1;
+        while (timer <= 5)
+        {
+            Console.WriteLine(timer);
+            timer++;
+        }
+
+        int[] arr = { 2, 4, 5, 1, 2, 4 };
+        int sum= 0;
+
+        foreach (int i in arr)
+        {
+            sum += i;
+        }
+    }
+}
