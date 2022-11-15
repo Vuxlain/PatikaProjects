bfa17871c3b58dae451309051662b0457e2da840
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-15 23:06:03 +0300

    Add files via upload

---
+++ PatikaProject/Try-Catch/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try{
+            Console.WriteLine("Bir sayi giriniz.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiginiz sayi: " + num);
+           
+        }
+        catch(Exception ex) {
+            Console.WriteLine("Hata: "+ex.Message.ToString());
+        }
+        finally { 
+            Console.WriteLine("İslem Tamamlandi."); 
+        }
+    }
+
+}
