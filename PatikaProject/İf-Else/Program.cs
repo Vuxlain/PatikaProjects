7c4e3df37a92325194f0f6fe9bee286ecbb29478
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-15 23:13:41 +0300

    Add files via upload

---
+++ PatikaProject/İf-Else/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1 ile 10 arasinda bir sayi giriniz.");
+        int num =Convert.ToInt32(Console.ReadLine());
+        if (num <= 5)
+        {
+            Console.WriteLine("Sayi besten kücük");
+        }
+        else
+        {
+            Console.WriteLine("Sayi besten büyük");
+
+        }
+
+    }
+}
