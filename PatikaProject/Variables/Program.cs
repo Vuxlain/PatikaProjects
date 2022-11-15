fb28cd4446302ef849819a62715f7bdf0abf2701
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-15 18:12:12 +0300

    Add files via upload

---
+++ PatikaProject/Variables/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte b = 5;
+        sbyte c = 5;
+
+        short s = 5;
+        ushort us = 5;
+        int i = 2;
+
+        uint ui = 2;
+        long l = 4;
+        ulong ul = 4;
+
+        float f = 5;
+        double d = 5;
+        decimal de = 5;
+
+        char ch = '2';
+        string str = "Kerem";
+
+        bool b1 = true;
+        bool b2 = false;
+
+        DateTime dt = DateTime.Now;
+
+        Console.WriteLine(dt);
+        Console.WriteLine(b + "--" + c + "--" + s + "--" + us + "--" + i + "--" + ui + "--" + l + "--" + ul + "--" + ch + "--" + d + "--" + ch + "--" + str + "--" + b1);
+
+
+
+
+
+    }
+}
