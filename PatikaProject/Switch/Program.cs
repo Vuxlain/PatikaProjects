8778d1060cfb50d000cebf1dfdb4443105ea9eb9
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-15 23:29:55 +0300

    Add files via upload

---
+++ PatikaProject/Switch/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1 yada 0 sayilarından birini giriniz.");
+        int num =Convert.ToInt32(Console.ReadLine());
+        switch (num )
+        {
+            case 1:
+                Console.WriteLine("Sayi: " + num);
+                break;
+            case 0:
+                Console.WriteLine("Sayi: " + num);
+                break;
+            default:
+                Console.WriteLine("Gecersiz Deger");
+                break;
+                
+        }
+
+    }
+}
