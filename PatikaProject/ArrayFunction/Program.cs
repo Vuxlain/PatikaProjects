6ba9a67a02ea3af92c0913244ab684e5d4853206
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-17 12:34:26 +0300

    Add files via upload

---
+++ PatikaProject/ArrayFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 5,9,7,15,4,2,9,6,44,1,3,64,51};
+        foreach (var i in arr)
+        {
+            Console.Write(i+"-");
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine("**********************");
+        Array.Sort(arr);
+        Array.Reverse(arr);
+        foreach (var i in arr)
+        {
+            Console.Write(i+"-");
+        }
+        Console.WriteLine();
+        Console.WriteLine("5'in indexi: "+Array.IndexOf(arr,5));
+    }
+}
