cfa16cf05ba34976b644469f24f3e73816e477e2
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-19 00:27:24 +0300

    Add files via upload

---
+++ PatikaProject/Overload/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 42;
+        int b = 20;
+        int c = 15;
+        Console.WriteLine(sum(a, b));
+        Console.WriteLine(sum(a, b,c));
+
+    }
+
+
+    static int sum(int a,int b)
+    {
+        return a + b;
+    }
+
+    static int sum(int a, int b,int c)
+    {
+        return a + b+cs;
+    }
+}
