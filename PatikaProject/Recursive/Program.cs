5ecb1254a88f2d0115c918d329efadbf969faa1f
Mikdat Kerem Kalkan <94854667+Vuxlain@users.noreply.github.com>
2022-11-19 00:33:40 +0300

    Add files via upload

---
+++ PatikaProject/Recursive/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Fibonacci kaca kadar gitsin: \n");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+        fibonacci(1,1,1,num);
+    }
+
+    static int fibonacci(int a, int b, int temp, int last)
+    {
+
+        temp = a;
+        if (a == 1)
+        {
+            Console.WriteLine(1);
+        }
+
+        if (a > last)
+        {
+            return 0;
+        }
+
+        Console.WriteLine(a);
+
+        a += b;
+        b = temp;
+        temp = a;
+
+        return (fibonacci(a, b, temp, last));
+    }
+}
